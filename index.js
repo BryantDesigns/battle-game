bc6e2a71192760315fdfa7d4c5ef267864a28c84
Tyler Bryant <tbryantdesignsllc@gmail.com>
2022-02-23 10:45:35 -0700

    More refactor

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const hero = {
   name: 'Wizard',
   avatar: 'images/wizard.png',
   health: 60,
-  diceRoll: 6,
+  diceRoll: [3, 1, 4],
+  diceCount: 3,
 };
 
 const monster = {
@@ -11,19 +12,23 @@ const monster = {
   name: 'Orc',
   avatar: 'images/orc.png',
   health: 10,
-  diceRoll: 4,
+  diceRoll: [6],
+  diceCount: 1,
 };
 
-
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceRoll } = data;
-  document.getElementById(
-    elementId
-  ).innerHTML = `<div class="character-card">
+  const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+  const diceHtml = diceRoll.map(die => {
+    return `<div class="dice">${die}</div>`;
+  }).join('');
+
+  document.getElementById(elementId).innerHTML = `<div class="character-card">
         <h4 class="name"> ${name} </h4>
         <img class="avatar" src='${avatar}'/>
         <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
+        <div class="dice-container">
+                ${diceHtml}
+            </div>
     </div>`;
 }
 renderCharacter(hero);
